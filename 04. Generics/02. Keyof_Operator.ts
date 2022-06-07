f2d9857459f6cfad143e3fcaf482cc44a52ae86a
Hycient195 <onyeukwuhycient@gmail.com>
2022-06-07 14:57:45 +0100

    Added 02. Keyof_operator

--- 04. Generics/02. Keyof_Operator.ts
+++ 04. Generics/02. Keyof_Operator.ts
@@ -28,23 +28,35 @@ type diskParamsKeys = keyof diskParams;
 
 // type diskParamsKeys = "size" | "number" | "isErasable";
 
-// Let's take a practical example of the use of the keyof operator
+/**
+ * One Could ask what is the need of this when we can always define a new type with values
+ * corresponding to the string values of the properties of another type, well, the main use 
+ * of this is for flexibility and reusability sake.
+ *  A good practice while working on large codebases is to avoid redundant procedures in carrying
+ * out process. Automating actions and processes is a good way to go about this.
+ *  The "keyof" operator helps out in this, so that for every time the properties of the 
+ * reference object is updated or modified, the typs(s) making use of its properties are also
+ * modified in the same suit.
+ * The keyof Operator helps to keep a funciton signature in sync with a type or interface.
+ */
 
+// Let's take a practical example of a usecase of the keyof operator.
 
-
+/*==========================================*/
+/* Using The keyof Operator with a function */
+/*==========================================*/
   /* Example */
 type customObj = {
   address: string,
   email: string
 }
 
-type customObjParams = keyof customObj;
-
-function replacePropFromObj(obj: customObj, param: customObjParams, value: string ): customObj{
+function replacePropFromObj(obj: customObj, param: keyof customObj, value: string ): customObj{
   const newObj = {...obj}
   newObj[param] = value;
   return newObj;
 }
+// The function above though can be made more dynamic by making it generic;
 
 const entity = {
   address: "oldAddress",
@@ -54,49 +66,82 @@ const entity = {
 const mutatedObj = replacePropFromObj(entity, "address", "newAddress")
 console.log(mutatedObj);
 
+/**
+ * This example above takes an object, a property from that object to be modified, and the 
+ * new value to modify the old value of the property.
+ * 
+ *  The To ensure that the property specified to be modified is always a property contained
+ * in the object specified, the keyof Operator is used to extract the keys/properties of the 
+ * object and assign them to another type. This type now becomes the explcit union of types of the
+ * second argument to the function (param), such that if a property is specified to be modified,
+ * and is not property of the object specified, and error would be thrown. You gerrit??
+*/
 
 
 
 
 
-// const obj:{
-//   firstName: string,
-//   lastName: string,
-//   age: number,
-//   isActive: boolean
-// } = {
-//   firstName: "Mile",
-//   lastName: "Monroe",
-//   age: 43,
-//   isActive: false
-// }
+/*========================================*/
+/* Using the Keyof Operator With Generics */
+/*========================================*/
+/**
+ * When a propery of a function is dependent the properties of another "type", "object", or
+ * "interface", then these properties can be made type safe by using a generic function, and 
+ * extracting the properties from the type, object or interface by using the "extends" 
+ * keyword as shown in the example below.
+ * 
+*/
 
-
-type obj2 = {
-  firstName: string,
-  lastName: string,
-  age: number,
-  isActive: boolean
+function extractProp<Obj, Prop extends keyof Obj>(user: Obj, prop: Prop): Obj[Prop]{
+  let newObj = { ...user }
+  return newObj[prop]
 }
 
-
-type keyType = keyof obj2;
-
-function doSomething(arg1: keyType){
-  return arg1;
+const newUser = {
+  name: "Shanks",
+  age: 50,
+  active: true
 }
 
-doSomething("lastName")
-
-
-
-
-
-
-/*=================*/
-/* Common Pitfalls */
-/*=================*/
+console.log(extractProp(newUser, "name"));
 
 /**
- * 1. A explicitly defined type cannot be logged unto console.
+ * "<Obj, Prop extends keyof Obj>"" means that the type values for anything assigned to "Prop", 
+ * must only be a union type of string representation of the properties of Obj. If a string 
+ * that is not a part of that union type is passed to the function above, or assigned to the
+ * value of anything assigned to prop, then an error is thrown.
+ * 
+ *  Is is also noteworthy that the type being extended can also be derived from an explicit
+ * type or an interface.
 */
+
+
+
+
+
+                        /*=====*/
+                        /*Hints*/
+                        /*=====*/
+/**
+ * 1. click on an entity In VSCode ( object, class or even ) use CTRL + Spacebar on it
+ *    and the list of properties and methods on that entity would be exposed.
+*/
+
+
+                    /*=================*/
+                    /* Common Pitfalls */
+                    /*=================*/
+/**
+ * 1. An explicitly defined type cannot be logged unto console, likewise a union of types
+ *    obtained using the keyof operator. 
+ * 2. The generic type must not be specified, when invoking a function extracting parameters
+ *    using the keyof operator in a generic initialization.
+*/
+
+
+
+
+
+
+
+
